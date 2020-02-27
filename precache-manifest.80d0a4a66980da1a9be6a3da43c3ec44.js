906c444ccd09fce5ecc8ad25a9c7f4b707010c96
Max <LentyaiPrOO@yandex.ru>
2020-02-27 20:55:36 +0300

    Updates

---
+++ precache-manifest.80d0a4a66980da1a9be6a3da43c3ec44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3227a21c4692f9423033125cda15811c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a358aee9fd6faff38bb",
+    "url": "/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "5d137aaedff2e31ce486",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a358aee9fd6faff38bb",
+    "url": "/static/js/2.19663239.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.19663239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d137aaedff2e31ce486",
+    "url": "/static/js/main.b5a77350.chunk.js"
+  },
+  {
+    "revision": "33dcbad9c9b493eb7325",
+    "url": "/static/js/runtime-main.b2d992f3.js"
+  }
+]);
