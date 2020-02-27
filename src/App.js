d650b0c8d4639b24028ae7621606e453e028b83b
Max <LentyaiPrOO@yandex.ru>
2020-02-27 20:51:35 +0300

    working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Table, Modal, Row, Col } from "antd";
+import "antd/dist/antd.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class App extends React.Component {
+  state = {
+    usersData: [],
+    modalVisible: false,
+    posts: []
+  };
+
+  columnsUsers = [
+    {
+      title: "Записи",
+      dataIndex: "id",
+      key: "id",
+      render: text => (
+        <a onClick={() => this.showUserPosts(text)}>Посмотреть записи</a>
+      )
+    },
+    {
+      title: "Имя",
+      dataIndex: "name",
+      key: "name"
+    },
+    {
+      title: "Никнейм",
+      dataIndex: "username",
+      key: "username"
+    },
+    {
+      title: "Почта",
+      dataIndex: "email",
+      key: "email"
+    },
+    {
+      title: "Телефон",
+      dataIndex: "phone",
+      key: "phone"
+    }
+  ];
+
+  columnsPosts = [
+    {
+      title: "Id",
+      dataIndex: "id",
+      key: "id"
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title"
+    },
+    {
+      title: "Body",
+      dataIndex: "body",
+      key: "body"
+    }
+  ];
+
+  async showUserPosts(id) {
+    this.setState({
+      modalVisible: true
+    });
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+    );
+    let posts = await response.json();
+    posts.forEach(item => {
+      item.key = item.id;
+    });
+    this.setState({ posts });
+  }
+
+  closePostsModal() {
+    this.setState(state => {
+      return {
+        modalVisible: false
+      };
+    });
+  }
+
+  async componentDidMount() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let users = await response.json();
+    users.forEach(item => {
+      item.key = item.id;
+    });
+    this.setState({ usersData: users });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Row>
+          <Col span={12} offset={6}>
+            <Table
+              dataSource={this.state.usersData}
+              columns={this.columnsUsers}
+            />
+            ;
+          </Col>
+        </Row>
+        <Modal
+          title="Записи пользователя"
+          visible={this.state.modalVisible}
+          onOk={this.closePostsModal.bind(this)}
+          onCancel={this.closePostsModal.bind(this)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Table dataSource={this.state.posts} columns={this.columnsPosts} />
+        </Modal>
+      </div>
+    );
+  }
 }
-
-export default App;
